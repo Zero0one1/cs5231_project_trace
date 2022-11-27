d365f0639e1461d1eb049db00dca9bc9c20f2873
Dethyl <113376382+Dethyl@users.noreply.github.com>
2022-11-28 02:53:48 +0800

    Self cleanup, more jumps and calls
    Strengthened capturing jumps and calls to improve forming the CFG. Added self-cleanup of temporary files created when running the python file.

--- code/CFG_prototype.py
+++ code/CFG_prototype.py
@@ -2,30 +2,57 @@ import pandas as pd
 import os, re
 
 READ_TRACE = "/home/cs5231project/Desktop/Peekaboo/read_trace"
-PPROLOGUE_FILTER = "/home/cs5231project/Desktop/cfg_pattern1.txt"
-EPILOGUE_FILTER = "/home/cs5231project/Desktop/cfg_pattern2.txt"
-TRACE_PATH = "/home/cs5231project/Desktop/Project/buffer_overflow_normal/2710"
-#TRACE_PATH = "/home/cs5231project/Desktop/Project/buffer_overflow_stacksmash/2866"
-PRO_RAW = "/home/cs5231project/Desktop/prologueLines.txt"
-EPI_RAW = "/home/cs5231project/Desktop/epilogueLines.txt"
-CFG_FILE = "/home/cs5231project/Desktop/cfg.txt"
+#TRACE_PATH = "/home/cs5231project/Desktop/Project/buffer_overflow_normal/2710"
+TRACE_PATH = "/home/cs5231project/Desktop/Project/buffer_overflow_stacksmash/2866"
+
+PPROLOGUE_FILTER = "./cfg_pattern1.txt"
+EPILOGUE_FILTER = "./cfg_pattern2.txt"
+CALL_FILTER = "./cfg_pattern3.txt"
+JMP_FILTER = "./cfg_pattern4.txt"
+PRO_RAW = "./prologueLines.txt"
+EPI_RAW = "./epilogueLines.txt"
+CALL_RAW = "./callLines.txt"
+JMP_RAW = "./jmpLines.txt"
+CFG_FILE = "./cfg.txt"
 
 instrRegex=re.compile("^\[[0-9]{6}\]")
 addrRegex=re.compile("0x[[a-f0-9]{12}")
 
 myColumns = ["Instruction_Num","Address","Opcode", "ASM_instruction"]
 cfg_DF = pd.DataFrame(columns=myColumns)
-#We want ENDBR64 (Intel CET), Prologue, Epilogue to form CFG
-opCodeList = ["\[f3 0f 1e fa\]", "\[55\]","\[48 89 e5\]", "\[48 83 ec", "\[c9\]", "\[c3\]"] 
-asmList = ["call 0x", "jmp"]
+#We wantform CFG, ENDBR64 (Intel CET), Prologue, Epilogue, various jumps [72 to 77 ??], call [e8 ??]
+opCodeList = ["\[f3 0f 1e fa\]", "\[55\]","\[48 89 e5\]", "\[48 83 ec", "\[c9\]", "\[c3\]","\[72","\[73","\[74","\[75","\[76","\[77", "\[e8"] 
 
 def GenerateIntermediateFiles():
     output_prologueTrace = READ_TRACE + " -p " + PPROLOGUE_FILTER + " " + TRACE_PATH + " > " + PRO_RAW
     output_epilogueTrace = READ_TRACE + " -p " + EPILOGUE_FILTER + " " + TRACE_PATH + " > " + EPI_RAW
+    output_callTrace = READ_TRACE + " -p " + EPILOGUE_FILTER + " " + TRACE_PATH + " > " + CALL_RAW
+    output_jmpTrace = READ_TRACE + " -p " + EPILOGUE_FILTER + " " + TRACE_PATH + " > " + JMP_RAW
     print(output_prologueTrace)
     os.system(output_prologueTrace)
+    CFGparseFile(PRO_RAW)
     print(output_epilogueTrace)
     os.system(output_epilogueTrace)
+    CFGparseFile(EPI_RAW)        
+    print(output_callTrace)
+    os.system(output_callTrace)
+    CFGparseFile(CALL_RAW)        
+    # 72 ?? jb (jmp if below); # 73 ?? jnb (jmp if not below); # 74 ?? je (jmp if equal); 
+    # 75 ?? jne (jmp if not equal); # 76 ?? jbe (jmp if below or equal); # 77 ?? ja (jmp if above);
+    count = 72
+    while count < 78:
+        f = open(JMP_FILTER,"w")
+        f.write(str(count) + " ??")
+        f.close()
+        print(output_jmpTrace)
+        os.system(output_jmpTrace)
+        CFGparseFile(JMP_RAW)        
+        count = count + 1
+    os.remove(PRO_RAW) #start cleanup
+    os.remove(EPI_RAW)
+    os.remove(CALL_RAW)
+    os.remove(JMP_RAW)
+    os.remove(JMP_FILTER)
 
 def CFGparseFile(filname):
     global cfg_DF
@@ -49,17 +76,15 @@ def CFGparseFile(filname):
             currASM = tmpStr[1]
             currASM = currASM.strip()
             currASM = currASM.replace('\t',' ')           
-            print(currInstrNum, currAddr, currOpcode, currASM)   
+            #print(currInstrNum, currAddr, currOpcode, currASM)   
             tmpDF = pd.DataFrame([[currInstrNum,currAddr,currOpcode,currASM]], columns=myColumns)
             cfg_DF = pd.concat([cfg_DF,tmpDF])
         
 def CFGparseRelevantFilesAndProcess():
     global cfg_DF
-    CFGparseFile(PRO_RAW)
-    CFGparseFile(EPI_RAW)        
     cfg_DF.sort_values(["Instruction_Num"]) 
     cfg_DF.drop_duplicates(keep='first',inplace=True) #there's some overlap with libpeekaboo searches by pattern file
-    cfg_DF = cfg_DF[(cfg_DF['Opcode'].str.contains('|'.join(opCodeList))) | (cfg_DF['ASM_instruction'].str.contains('|'.join(asmList)))]
+    cfg_DF = cfg_DF[(cfg_DF['Opcode'].str.contains('|'.join(opCodeList)))]
     cfg_DF.to_csv(CFG_FILE, sep='\t', na_rep='', header=True, index=False, encoding=None, date_format=None, doublequote=True)
     
 def main():
